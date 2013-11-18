6aece50f480698b32717d1b87a512a18cdfc2550
Olav Nymoen <olav@comoyo.com>
2013-11-18 12:00:58 +0100

    Too many changes?
    Contains:
    
    - All panels changed by hashchange
    - CSS back to being flexbox based
    - only initialize panels / panel javascript once
    - pub/sub for params shared between panels (Could use DOM listener
      instead)
    - Navigation back to the panels
    
    Broke:
    - Not fixed next / prev enabled / disabled (should be handled within the
      panel)
    - Some duplication / oddness left when changing to self contained
      panels.

--- apps/system/fxa/js/screens/fxam_enter_email.js
+++ apps/system/fxa/js/screens/fxam_enter_email.js
@@ -22,20 +22,19 @@ FxaModuleEnterEmail = (function() {
 
   function _enableNext(emailEl) {
     if (_isEmailValid(emailEl)) {
-      FxaModuleUI.enableNextButton();
+      // document.querySelector('.right').removeAttribute('disabled');
+      // FxaModuleUI.enableNextButton();
     } else {
-      FxaModuleUI.disableNextButton();
+      // document.querySelector('.right').setAttribute('disabled', 'disabled');
+      //FxaModuleUI.disableNextButton();
     }
   }
 
   var Module = Object.create(FxaModule);
   Module.init = function() {
+    this.initNav();
     // Blocks the navigation until check the condition
     _enableNext(this.fxaEmailInput);
-
-    if (this.initialized) {
-      return;
-    }
 
     // Cache HTML elements
     this.importElements('fxa-email-input');
@@ -44,11 +43,14 @@ FxaModuleEnterEmail = (function() {
       'input',
       function onInput(event) {
         _enableNext(event.target);
-      }
+      }, false
     );
 
-    // Avoid to add listener twice
-    this.initialized = true;
+    var self = this;
+    this.onChange('email', function(email) {
+      if (email)
+        self.fxaEmailInput.value = email;
+    });
   };
 
   Module.onNext = function onNext(gotoNextStepCallback) {
@@ -56,11 +58,13 @@ FxaModuleEnterEmail = (function() {
 
     var email = this.fxaEmailInput.value;
 
+    var self = this;
     FxModuleServerRequest.checkEmail(
       email,
       function onServerResponse(response) {
         FxaModuleOverlay.hide();
         FxaModuleManager.setParam('email', email);
+        self.set('email', email);
         if (response.registered) {
           _loadSignIn(gotoNextStepCallback);
         } else {
@@ -76,11 +80,8 @@ FxaModuleEnterEmail = (function() {
     );
   };
 
-  Module.onBack = function onBack() {
-    FxaModuleUI.enableNextButton();
-  };
-
   return Module;
 
 }());
 
+FxaModuleEnterEmail.init();
